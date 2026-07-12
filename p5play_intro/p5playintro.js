908400770504f20715ab5da5273e2b3849c99bc3
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-07-12 04:21:24 +0000

    Jul 12, 2026, 4:21 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -18,9 +18,10 @@ function setup() {
    ball.color="blue"
 
    box = new Sprite();
-   box.x=100
-   box.y=100
-   box.w=50
+   box.x=100;
+   box.y=100;
+   box.w=50;
+   box.h=50
   }
 
 function draw() {
