ef4a008045ee23326f9c92e90240995597cabfe0
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-07-19 03:56:47 +0000

    Jul 19, 2026, 3:56 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -11,7 +11,7 @@ function setup() {
   // write your codes here
 let sum=0
 for(let i=2;i<=20;i+=2){
-  text()
+  text(i+",")
 }  // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here
 
