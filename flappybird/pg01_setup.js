9bd2a921ca884c61378dec96fd24bc1530f713dd
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-02 04:03:46 +0000

    Aug 2, 2026, 4:03 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -20,7 +20,7 @@ function setup(){
     bird.collider = "dynamic";
     bird.mass = 2;
     bird.drag = 0.02;
-    bird.bounciness = ;
+    bird.bounciness = 1;
     world.gravity.y = 10;
 
     floor = new Sprite();
