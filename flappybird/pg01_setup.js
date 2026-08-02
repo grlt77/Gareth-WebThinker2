395b3fc1cb24f497e64780186ddbbb4fe9d9ffd3
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-02 04:17:31 +0000

    Aug 2, 2026, 4:17 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -43,6 +43,7 @@ function draw(){
     fill("blue");
     textSize(14);
     text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
-    text('isMoving: ' + bird)
+    text('isMoving: ' + bird.isMoving,10,40)
+    
 
 }
