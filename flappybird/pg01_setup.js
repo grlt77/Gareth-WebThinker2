1a1fc73171883e33bb19f40ba11972501c3ee2c5
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-02 04:19:08 +0000

    Aug 2, 2026, 4:19 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -36,7 +36,7 @@ function draw(){
     image(bg,0,0,width,height);
 
     if (kb.presses('space')){
-        bird.vel.y = -10000;
+        bird.vel.y = -100;
         bird.sleeping = false;
     }
 
