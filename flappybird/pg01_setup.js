967f4ebac83c4a5522589a77172d0c9fa33058c0
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-02 04:01:48 +0000

    Aug 2, 2026, 4:01 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -27,7 +27,7 @@ function setup(){
     floor.x = 200;
     floor.y = height - 20;
     floor.width = 400;
-    floor.height = 1200;
+    floor.height = 1100;
     floor.collider = "static";
     floor.img = base;
 }
