60b55fcf3fe98c705e0cf25c238d5626a6348af6
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-16 04:24:49 +0000

    Aug 16, 2026, 4:24 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -1,5 +1,6 @@
 let bird, floor; 
 let flapMidImg, bg, base;
+let pipeGroup, pipe, bottomPipe, topPipe;
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
@@ -7,6 +8,8 @@ function preload() {
     flapDownImg = loadImage('assets/yellowbird-downflap.png');
     bg = loadImage('assets/background-night.png');
     base = loadImage('assets/base.png');
+    pipe = loadImage('assets/pipe-green.png');
+
 }
 
 function setup(){
@@ -32,6 +35,8 @@ function setup(){
     floor.height = 125;
     floor.collider = "static";
     floor.img = base;
+
+    pipeGroup = new Group();
 }
 
 function draw(){
@@ -60,4 +65,25 @@ function draw(){
         bird.img = flapMidImg;
         bird.rotation = 0;
     }
+    
+    if (frameCount === 1){
+        spawnPipePair();
+    }
+}
+
+function spawnPipePair(){
+    let gap = 50;
+    let midY = height / 2;
+
+    bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'static');
+    bottomPipe.img = pipe;
+
+    pipeGroup.add(bottomPipe);
+    pipeGroup.layer = 0;
+
+    topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, 'static');
+    topPipe.img = pipe;
+    topPipe.rotation = 180;
+
+    pipeGroup.add(topPipe);
 }
