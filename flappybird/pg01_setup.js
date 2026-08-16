e928990cc15b679e646393deb71743520b114946
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-16 04:10:37 +0000

    Aug 16, 2026, 4:10 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -1,6 +1,6 @@
 let bird, floor; 
 let flapMidImg, bg, base;
-let pipeGroup, pipe
+let pipeGroup, pipe, bottomPipe;
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
