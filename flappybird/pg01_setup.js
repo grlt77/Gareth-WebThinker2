26b1185ba874873420863169576a0ef4e2717f19
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-16 04:23:19 +0000

    Aug 16, 2026, 4:23 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -83,5 +83,7 @@ function spawnPipePair(){
 
     topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, 'static');
     topPipe.img = pipe;
-    topPipe 
+    topPipe.rotation = 180;
+
+    pipe
 }
