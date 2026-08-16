2f21467272678d396733b22325c6cef005ebb643
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-16 03:50:51 +0000

    Aug 16, 2026, 3:50 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -3,7 +3,8 @@ let flapMidImg, bg, base;
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
-    flapUpImg = loadImage('assets/yellowbird-midflap.png');
+    flapUpImg = loadImage('assets/yellowbird-upflap.png');
+        flapMidImg = loadImage('assets/yellowbird-midflap.png');
     bg = loadImage('assets/background-night.png');
     base = loadImage('assets/base.png');
 }
