47f4dc53a534f6d9892832706b1a1557571c92da
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-23 04:37:29 +0000

    Aug 23, 2026, 4:37 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -61,12 +61,12 @@ function draw(){
         }
 
         fill("blue");
-    textSize(14);
-    text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
-    text('isMoving: ' + bird.isMoving,10,40);
-    text('sleeping: ' + bird.sleeping,10,60);
+        textSize(14);
+        text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
+        text('isMoving: ' + bird.isMoving,10,40);
+        text('sleeping: ' + bird.sleeping,10,60);
 
-    if (bird.vel.y < -1){
+        if (bird.vel.y < -1){
         bird.img = flapUpImg;
         bird.rotation = -30;
     }
