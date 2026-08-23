8118cb7c487403aa8c5eb70ced10d9541fb5c08a
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-23 04:24:35 +0000

    Aug 23, 2026, 4:24 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -1,7 +1,7 @@
 let bird, floor; 
 let flapMidImg, bg, base;
 let pipeGroup, pipe, bottomPipe, topPipe;
-let gameoverImg, gameoverLabel, start;
+let gameoverImg, gameoverLabel, startScreenLabel;
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
