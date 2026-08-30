bdb61403c8db45e71ee86d44a26270d6800c605d
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-30 03:50:56 +0000

    Aug 30, 2026, 3:50 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -140,7 +140,7 @@ function spawnPipePair(){
     pipeGroup.add(topPipe);
 }
 
-function drawScore(x, y,){
+function drawScore(x, y, score, digitWidth,){
     scoreDigits.removeAll();
     let scoreStr = str(score);
     let totalWidth = scoreStr.length * digitWidth;
