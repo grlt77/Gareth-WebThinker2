46068a4e4d62f82db4ae32d4772cee884e6c2951
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-30 03:36:34 +0000

    Aug 30, 2026, 3:36 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -15,7 +15,9 @@ function preload() {
     pipe = loadImage('assets/pipe-green.png');
     gameoverImg = loadImage('assets/gameover.png');
     startScreenImg = loadImage('assets/message.png');
-    for (let i = 0; i < 10; i++)
+    for (let i = 0; i < 10; i++){
+        numberImages[i]
+    }
 
 }
 
