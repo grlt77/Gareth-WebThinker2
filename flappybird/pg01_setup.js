23b7c3d903d15898d3b56aa4e0885346c7f3abac
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-30 03:38:19 +0000

    Aug 30, 2026, 3:38 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -50,7 +50,8 @@ function setup(){
     startScreenLabel = new Sprite(width/2, height/2, 50, 50, 'none')
     startScreenLabel.img = startScreenImg;
 
-    scoreDigits = new Group
+    scoreDigits = new Group;
+    scoreDigits.collider = 'none
 }
 
 function draw(){
