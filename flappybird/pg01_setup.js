be1a2fefa401dceca37af1f3e3155c4076757e48
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-30 04:17:51 +0000

    Aug 30, 2026, 4:17 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -148,6 +148,6 @@ function drawScore(x, y, score, digitWidth, digitHeight){
         let xPos = startX + i * digitWidth;
         let digitSprite = new scoreDigits.Sprite(xPos, y, digitWidth, digitHeight);
         digitSprite.img = numberImages[digit];
-        moveGroup()
+        moveGroup(scoreDigits, )
     }
 }
