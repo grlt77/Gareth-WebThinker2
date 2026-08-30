eb7df9734a86e17f7e6eaf7e1b4da813f124b1d8
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-30 04:22:15 +0000

    Aug 30, 2026, 4:22 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -154,7 +154,9 @@ function drawScore(x, y, score, digitWidth, digitHeight){
     function moveGroup(group,targetX, spacing){
         let totalWidth = (group.length -1) * spacing;
         let startX = (targetX - totalWidth/2);
-        for (let i = 0; i < group.length; i++)
+        for (let i = 0; i < group.length; i++){
+            group[i].x
+        }
     }
 
 }
