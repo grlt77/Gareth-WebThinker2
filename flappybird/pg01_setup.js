417a1220293953c2d68b18265c14cdeee8817804
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-30 04:03:19 +0000

    Aug 30, 2026, 4:03 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -145,6 +145,6 @@ function drawScore(x, y, score, digitWidth, digitHeight){
     let startX = x - totalWidth / 2;
     for (let i =0; i < scoreStr.length; i++) {
         let digit = int(scoreStr[i]);
-        let xPos = startX
+        let xPos = startX + i * digit
     }
 }
