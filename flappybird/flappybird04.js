e0e4500a0a037e931ecafab0e95659ab140ef542
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-08-30 03:33:55 +0000

    Aug 30, 2026, 3:33 AM

--- flappybird/flappybird04.js
+++ flappybird/flappybird04.js
@@ -4,7 +4,7 @@ let pipeGroup, pipe, bottomPipe, topPipe;
 let gameoverImg, gameoverLabel, startScreenLabel, startScreenImg;
 let startGame = false;
 let score = 0;
-let 
+let numberImages = []
 
 function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
