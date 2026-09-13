0b5957efe4193da0c4e90b9a34baa038639c9b19
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-09-13 03:48:37 +0000

    Sep 13, 2026, 3:48 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -108,7 +108,7 @@ function draw(){
         }
 
         for (let pipe of pipeGroup) {
-            let pipeRightEdge = pipe.x + pipe.w
+            let pipeRightEdge = pipe.x + pipe.w / 2;
         }
 
         if (bird.collides(pipeGroup) || bird.collides(floor)){
