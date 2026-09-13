f9a1c29dda6e2ea7d520d8ccfc885823ffcb2d47
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-09-13 04:14:36 +0000

    Sep 13, 2026, 4:14 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -15,7 +15,7 @@ function preload() {
     pipe = loadImage('assets/pipe-green.png');
     gameoverImg = loadImage('assets/gameover.png');
     startScreenImg = loadImage('assets/message.png');
-    flapWingSnd = load
+    flapWingSnd = loadSound('flappybird/assets/sfx_wing.mp3')
     for (let i = 0; i < 10; i++){
         numberImages[i] = loadImage('assets/' + i + '.png');
     }
