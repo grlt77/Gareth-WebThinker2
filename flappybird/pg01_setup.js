148db33ac2d908c00ebca246c3188875cd7ba375
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-09-13 04:22:09 +0000

    Sep 13, 2026, 4:22 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -118,9 +118,9 @@ function draw(){
             let birdLeftEdge = bird.x - bird.w / 2;
 
             if (pipe.passed == false && pipeRightEdge < birdLeftEdge){
+                
                 pipe.passed = true;
                 score++;
-                plusPointSnd.play();
             }
         }
 
