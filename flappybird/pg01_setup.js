ffb62b700a560dbaf5599de8b539effb2ac09639
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-09-13 03:47:22 +0000

    Sep 13, 2026, 3:47 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -107,7 +107,7 @@ function draw(){
                 }
         }
 
-        
+        for (let pipe of )
 
         if (bird.collides(pipeGroup) || bird.collides(floor)){
             gameoverLabel = new Sprite(width/2, height/2, 192, 42);
