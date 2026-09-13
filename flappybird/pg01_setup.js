646c2cea0fa670db709db935fdca279bdfd3f9a4
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-09-13 04:23:45 +0000

    Sep 13, 2026, 4:23 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -102,7 +102,7 @@ function draw(){
         camera.x = bird.x;
         floor.x = bird.x;
 
-        if (frameCount % 60 === 0 ){
+        if (frameCount % 90 === 0 ){
             spawnPipePair();
         }
 
