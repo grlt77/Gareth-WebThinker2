fa5169d2028f6ee8713e3ead54c75005f7e43759
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-09-13 03:54:02 +0000

    Sep 13, 2026, 3:54 AM

--- flappybird/pg01_setup.js
+++ flappybird/pg01_setup.js
@@ -134,7 +134,7 @@ function draw(){
 
 function spawnPipePair(){
     let gap = 50;
-    let midY = random(250, height - 300)
+    let midY = random(250, height - 100)
 
     bottomPipe = new Sprite(bird.x + 400, midY + gap / 2 + 200, 52, 320, 'static');
     bottomPipe.img = pipe;
