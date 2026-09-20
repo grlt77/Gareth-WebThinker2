5757a0bc5cfcb7a64e6da332a738cea359f2c374
grlt77 <194495867+grlt77@users.noreply.github.com>
2026-09-20 04:33:29 +0000

    Sep 20, 2026, 4:33 AM

--- fruitninja/fruitninjaplayground.js
+++ fruitninja/fruitninjaplayground.js
@@ -11,7 +11,7 @@ function preload(){
     watermelon = {
         whole: loadImage('assets/watermelonwhole.png')
     };
-    peach['whole']
+    fruitTypes = 
 }
 function setup() {
     createCanvas(600, 400);
